9e0221f04c362c2c12fb0f2a66d50ef855b0c4ba
SophieHegarty <shegarty874@gmail.com>
2017-03-06 16:18:27 +0000

    Prepare for bronze challenge

--- BuggyControl.cs
+++ BuggyControl.cs
@@ -14,37 +14,51 @@ namespace SerialComms
 
         static List<String> archive = new List<String>();
         static SerialPort port;
+        static Track track;
 
         static void Main(string[] args)
         {
+            track = new Track();
+            track.setOrientationForBuggy(0, BuggyOrientation.Clockwise);
+            track.setSectionForBuggy(0, 1);
+
             port = new SerialPort();
             port.PortName = "COM7";
             port.BaudRate = 9600;
             port.Open();
 
-            if (XBee) {
+            if (XBee)
+            {
                 port.Write("+++");
                 Thread.Sleep(1100);
                 port.WriteLine("ATID 3304, CH C, CN");
                 Thread.Sleep(9000);
             }
-            
+
             port.DiscardInBuffer();
             port.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
 
-            while (true) {
+            while (true)
+            {
                 Console.WriteLine("> ");
 
                 String outmessage = Console.ReadLine();
-               
-                if (outmessage == "print archive") {
+
+                if (outmessage == "print archive")
+                {
                     Console.WriteLine();
 
-                    for (int i = 0; i < archive.Count; i++) {
+                    for (int i = 0; i < archive.Count; i++)
+                    {
                         Console.WriteLine(archive[i]);
                     }
 
-                }else{
+                }else if(outmessage == "Start Bronze")
+                {
+                    send("Run");
+                }
+                else
+                {
                     send(outmessage);
                 }
             }
@@ -57,6 +71,34 @@ namespace SerialComms
 
             archive.add(inmessage);
 
+            if(inmessage == "Detected Gantry 1")
+            {
+                track.setGantryForBuggy(0, 0);
+                printTrack();
+                send("leave gantry");
+                track.setSectionForBuggy(0, track.getNextSectionForBuggy(0,false));
+                printTrack();
+
+            }else if(inmessage == "Detected Gantry 2")
+            {
+                
+                track.setGantryForBuggy(0, 1);
+                printTrack();
+
+                send("leave gantry");
+                track.setSectionForBuggy(0, track.getNextSectionForBuggy(0, false));
+                printTrack();
+            }
+            else if(inmessage == "Detected Gantry 3")
+            {
+
+                track.setGantryForBuggy(0, 2);
+                printTrack();
+                send("leave gantry");
+                track.setSectionForBuggy(0, track.getNextSectionForBuggy(0, false));
+                printTrack();
+            }
+
             Console.WriteLine();
             Console.WriteLine("*" + inmessage);
             Console.Write("> ");
@@ -68,6 +110,13 @@ namespace SerialComms
 
             port.DiscardOutBuffer();
             port.WriteLine(message);
+        }
+
+        static void printTrack()
+        {
+            Console.WriteLine();
+            Console.Write(track.getMap());
+            Console.Write("> ");
         }
     }
 
